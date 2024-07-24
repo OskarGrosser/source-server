ffcb5bfbc881842a7e6762319ad5c91b995f362d
Oskar Grosser <mail@oskargrosser.com>
2024-07-24 23:58:19 +0200

    Add simple file-server

--- webserver/src/main.js
+++ webserver/src/main.js
@@ -1,4 +1,11 @@
-const handlers = [];
+import * as path from "https://deno.land/std@0.207.0/path/mod.ts";
+
+const docRoot = path.join(
+  path.dirname(path.fromFileUrl(import.meta.url)),
+  "../public"
+);
+
+const handlers = [serveSimple];
 Deno.serve({ port: 8080 }, async (request) => {
   for (const handler of handlers) {
     let useResponse = true;
@@ -14,3 +21,19 @@ Deno.serve({ port: 8080 }, async (request) => {
 
   return new Response("Client error", { status: 404 });
 });
+
+function serveSimple(request, skip) {
+  const url = new URL(request.url);
+  const ext = /\.\w+$/.exec(url.pathname)?.[0].substring(1) ?? "";
+
+  return Deno.open(path.join(docRoot, url.pathname))
+    .then(fsFile => {
+      const type = extMime.getMimeType(ext) ?? "text/plain";
+      const headers = new Headers({
+        "Content-Type": `${type}; charset=UTF-8`
+      });
+
+      return new Response(fsFile.readable, { status: 200, headers });
+    })
+    .catch(() => skip());
+}
