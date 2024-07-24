a57c9e475390cebfffec730be66bc024564d5f36
Oskar Grosser <mail@oskargrosser.com>
2024-07-24 23:55:57 +0200

    Add support for middlewares

--- webserver/src/main.js
+++ webserver/src/main.js
@@ -1,3 +1,16 @@
-Deno.serve({ port: 8080 }, (request) => {
+const handlers = [];
+Deno.serve({ port: 8080 }, async (request) => {
+  for (const handler of handlers) {
+    let useResponse = true;
+    const skip = () => {
+      useResponse = false;
+    };
+
+    const response = await handler(request, skip);
+    if (useResponse) {
+      return response;
+    }
+  }
+
   return new Response("Client error", { status: 404 });
 });
