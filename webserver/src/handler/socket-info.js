1cf6e9b0993923bd3e6bd1428d6dcbbfdad94395
Oskar Grosser <mail@oskargrosser.com>
2024-07-30 09:23:31 +0200

    Add support for file changes

--- webserver/src/handler/socket-info.js
+++ webserver/src/handler/socket-info.js
@@ -1,3 +1,6 @@
+import * as path from "https://deno.land/std@0.207.0/path/mod.ts";
+import { debounce } from "https://deno.land/std@0.224.0/async/debounce.ts";
+import { getDocRoot } from "../docroot.js";
 import getDateTimeUtc from "../get-datetime-utc.js";
 import getInfo from "../get-info.js";
 
@@ -11,20 +14,33 @@ export default function socketInfo(request, skip) {
 
   const id = idCounter++;
   let intervalId;
+  const watcher = Deno.watchFs(path.join(getDocRoot(), "../resources/"));
+  const sendUpdate = debounce(() => {
+    socket.send(JSON.stringify(getInfo()));
+  }, 50);
 
-  socket.addEventListener("open", () => {
+  socket.addEventListener("open", async () => {
     console.log(`Client ${id} connected`);
 
+    // Initialize
     socket.send(JSON.stringify(getInfo()));
+
+    // Update
     intervalId = setInterval(() => {
       socket.send(JSON.stringify({
         datetime: getDateTimeUtc()
       }));
     }, 250);
+    for await (const event of watcher) {
+      if (event.kind !== "access") {
+        sendUpdate();
+      }
+    }
   });
   socket.addEventListener("close", () => {
     console.log(`Client ${id} disconnected`);
     clearInterval(intervalId);
+    watcher.close();
   });
 
   return response;
